b92afc2056261a25d5a89ec8cca66f76b423533f
luis m. sullen <sullen@luiss-MacBook-Pro.local>
2022-06-25 18:41:15 -0500

    add styles

--- src/modules/Subscription/Subscription.jsx
+++ src/modules/Subscription/Subscription.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import './Subscription.css'
 
 const Subscription = () => {
+
+
   return (
-    <div className="d-flex justify-content-center">
+    <div className="d-flex justify-content-center sub">
       <div>
-        <h3>¡Unete a nuestras novedades y promociones!</h3>
-        <div className="d-flex justify-content-center">
+        <h3 className="label mt-3">¡Unete a nuestras novedades y promociones!</h3>
+        <div className="d-flex justify-content-center mb-5 mt-4">
           <form action="." className="row">
             <div className="col-auto">
-              <input type="text" placeholder="email" />
+              <input type="text" placeholder="email" className="form-control"/>
             </div>
-            <div className="col-auto">
-              <input type="text" placeholder="Nombre" />
+            <div className="col-auto ">
+              <input type="text" placeholder="Nombre"  className="form-control"/>
             </div>
+            <button className="col-auto btn btn-dark" id="boton">
+                Subscribe!
+            </button>
           </form>
         </div>
       </div>
